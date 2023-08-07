dfec456a6aee669c5f5a821e065abd15d05bad10
#kkk <118553163+M7mmdddd@users.noreply.github.com>
2023-08-07 12:08:30 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# discord-music-bot-v14
+## Discord V14 Music Bot
+
+## 📑 Bot Performance
+-Music System
+-MessageButton
+-Slash Commands
+## 🔗 Platform
+![image](https://user-images.githubusercontent.com/93944142/196050222-640d6c0e-da1d-46c7-95d8-544f66a997cf.png)
+## 📷 Image
+![image](https://cdn.discordapp.com/attachments/1135064779581554779/1138140540265832488/image.png)
+## 🚨 Support Server
+✈ [Raven](https://discord.gg/187)
+
---
+++ index.js
@@ -0,0 +1,84 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { token } = require('./config.json');
+const { DeployCommands } = require('./deploy-commands');
+const mongoose = require('mongoose');
+const { DisTube } = require('distube');
+const fs = require('fs');
+require('colors');
+require("dotenv").config();
+const process = require ('node:process');
+
+process.on('unhandledRejection', (reason, promise) =>{
+console.log ('unhandeledRejection at:', promise, 'reason', reason)
+})
+
+
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+] });
+client.distube = new DisTube(client, {
+    emitNewSongOnly: false,
+    leaveOnEmpty: false,
+    leaveOnFinish: false,
+    leaveOnStop: false,
+    savePreviousSongs: true,
+    emitAddSongWhenCreatingQueue: false,
+    searchSongs: 10,
+    nsfw: false,
+    emptyCooldown: 25
+});
+
+(async () => {
+    await DeployCommands(); // Deploy commands
+
+    // متحكم ب event
+    const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js')); 
+    for (const file of eventFiles) {
+        const event = require(`./events/${file}`);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args, client));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args, client));
+        }
+    }
+
+    // المتحكم ب جميع الكومداندات
+    client.commands = new Collection();
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+
+    let commandNameLength = 0;
+    for (const command of client.commands) {
+        if (command[0].length > commandNameLength) {
+            commandNameLength = command[0].length;
+        }
+    }
+
+    for (const command of client.commands) {
+        console.log(`[COMMAND] ${command[0].padEnd(commandNameLength)} | ${'Loaded!'.green}`.gray); 
+    }
+
+    if (!fs.existsSync('./errors')) {
+        fs.mkdirSync('./errors');
+    }
+
+    if (!fs.existsSync('./database')) {
+        fs.mkdirSync('./database');
+    	};
+
+	mongoose.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).then(() => {
+		console.log('Connected to MongoDB')
+	}).catch((error) => console.error(error));
+ 
+    client.login(token); // bot log in 
+})();
